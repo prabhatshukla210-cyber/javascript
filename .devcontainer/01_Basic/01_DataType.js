467aacc468108c19f9083fe0470c5b91f8d5bca1
Prabhat Shukla <prabhatshukla210@gmail.com>
2026-05-25 14:32:47 +0000

    Added devcontainersetup and dependabot config

---
+++ .devcontainer/01_Basic/01_DataType.js
@@ -0,0 +1,26 @@
+"use strict";   //treat all JS code AS newer version 
+
+
+
+
+
+
+let age ;   // yeh undefind hai 
+
+//Data types  Primitive 
+
+//Number => 2ki power 53
+//Bigint        =====   jo ki stock market me data int me use krte h 
+//String =" "
+//Boolean => true/ false 
+// null =>standalonne value 
+// undefind 
+
+
+//object 
+
+console.log(typeof"Prabhat ");  // string
+console.log(typeof  undefined);  // undefind 
+console.log(typeof null);  // object 
+
+
